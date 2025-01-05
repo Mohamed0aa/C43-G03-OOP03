3f43a1b6662b7c14ed51b7165b323c7b4900495d
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-05 19:49:25 +0200

    v1

---
+++ demo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace demo
+{
+    internal class Program
+    {
+        static int sum(int a,int b)
+        {
+            return a + b;
+        }
+        static double sum(double a, int b)
+        {
+            return a + b;
+        }
+        static int sum(int a, int b,int c)
+        {
+            return a + b+c;
+        }
+        static void Main(string[] args)
+        {
+            #region vedio 1
+              //over loading using same function with diffrent task and [type of paramter and order]
+            dynamic result =sum(1,2);
+            Console.WriteLine(result);
+
+             result = sum(1,2,3);
+            Console.WriteLine(result);
+            #endregion
+        }
+    }
+}
