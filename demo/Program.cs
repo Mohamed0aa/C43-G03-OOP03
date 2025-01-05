8057703ae8634746c6d979b3269dac86e60714e6
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-05 20:21:52 +0200

    v2

--- demo/Program.cs
+++ demo/Program.cs
@@ -16,13 +16,21 @@
         }
         static void Main(string[] args)
         {
-            #region vedio 1
+            #region vedio 1 
               //over loading using same function with diffrent task and [type of paramter and order]
             dynamic result =sum(1,2);
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
 
              result = sum(1,2,3);
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region vedio 2 binary operator
+            complex c1 =new complex() { real = 1, imag = 2 };
+            complex c2 = new complex() { real = 1, imag = 2 };
+            complex c3;
+            c3 = c1 + c2;
+            Console.WriteLine(c3);
             #endregion
         }
     }
