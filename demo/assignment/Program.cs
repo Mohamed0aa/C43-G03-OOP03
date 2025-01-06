75694c22438c58ffc2d3bca594b91a3bf4fdf485
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-06 06:22:53 +0200

    q2

--- demo/assignment/Program.cs
+++ demo/assignment/Program.cs
@@ -10,6 +10,12 @@
             //Console.WriteLine(c.sum(1, 2));
             //Console.WriteLine(c.sum(1, 0.5));
             #endregion
+
+            #region q2 : Create a class named Rectangle with the following constructors
+            //rectangle rec = new rectangle(1,2);
+            //rectangle rec1 = new rectangle(1, 2);
+            //rectangle rec2 = new rectangle(5);
+            #endregion
         }
     }
 }
