53b8842a10b081d14ba2c89777a507c06b438789
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-06 06:16:25 +0200

    q1

---
+++ demo/assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1 : Write a class named Calculator that contains a method named Add. Overload the Add method to:
+            //calculator c = new calculator();
+            //Console.WriteLine(c.sum(1, 2, 3));
+            //Console.WriteLine(c.sum(1, 2));
+            //Console.WriteLine(c.sum(1, 0.5));
+            #endregion
+        }
+    }
+}
---
+++ demo/assignment/calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class calculator
+    {
+
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int sum(int x, int y,int z)
+        {
+            return x + y+z;
+        }
+        public double sum(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
